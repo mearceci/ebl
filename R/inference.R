74e8a0efdbaea1602b7f412a83a9298e4bb4b485
mearceci <43208974+mearceci@users.noreply.github.com>
2020-04-04 17:43:48 +0900

    add function

---
+++ R/inference.R
@@ -0,0 +1,16 @@
+#'mom
+#'
+#'Task
+#'
+#'@param data
+#'@return gammna
+#'@export
+mom_gamma <- function(sample_g){
+  x_bar <- mean(sample_g)
+  x2_bar <- mean(sample_g^2)
+  mom_theta <- (x2_bar - x_bar^2) / x_bar
+  mom_k <- x_bar / mom_theta
+
+  list(k_hat = mom_k, theta_hat = mom_theta)
+  }
+
